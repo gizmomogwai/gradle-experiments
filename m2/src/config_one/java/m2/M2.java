d0f95d2a40e8f9ddfefcebe784b43dd8f64174e0
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-11-25 13:18:01 +0100

    added m1, m2 and main modules

---
+++ m2/src/config_one/java/m2/M2.java
@@ -0,0 +1,6 @@
+package m1;
+public class M1 {
+    public static void m1() {
+        System.out.println("m1");
+    }
+}
---
+++ main/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import m1.M1;
+import m2.M2;
+
+public class Main {
+    public static void main(String[] args) {
+        M1.call();
+        M2.call();
+    }
+}

8eddf2dcb85a203e9dfeab7b4510bac486d287c5
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-11-26 11:10:17 +0100

    Added common sources to module m1 and run task

---
+++ m1/src/main/java/m1/M1Common.java
@@ -0,0 +1,7 @@
+package m1;
+
+public class M1Common {
+    public static void m1Common() {
+        System.out.println("M1Common.m1Common");
+    }
+}
--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import m1.M1;
+import m1.M1Common;
 import m2.M2;
 
 public class Main {
     public static void main(String[] args) {
+        M1Common.m1Common();
         M1.m1();
         M2.m2();
     }
